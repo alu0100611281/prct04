2dfa4fd724c72f894e00d0ff212956bf3451bf06
Jose Luis González Hernández <alu0100611281@ull.edu.es>
2013-10-05 12:45:49 +0100

    version suma

--- matriz.rb
+++ matriz.rb
@@ -3,6 +3,7 @@
 a = [[1, 4, 2], [6, 0, 3], [3, 5, 2]] 
 b = [[0, 6, 4], [8, 6, 2], [7, 9, 1]] 
 c = Array.new
+p = Array.new
 m, n = a.size, a[1].size
 
 for i in 0...m do
@@ -14,3 +15,25 @@ for i in 0...m do
   end
 end
 
+
+#SUMA DE MATRIZES.
+for i in 0...m do
+  for j in 0...n do
+    c[i][j] = a[i][j] + b[i][j]
+    end
+end
+
+puts "\n La Matriz a es = "
+a.each do |fila|
+  puts fila.join(" ")
+end
+
+puts "\n La Matriz b es = "
+b.each do |fila|
+  puts fila.join(" ")
+  end
+  puts "\n La Suma de la Matriz a y b es = "
+  c.each do |fila|
+    puts fila.join(" ")
+  end
+  
